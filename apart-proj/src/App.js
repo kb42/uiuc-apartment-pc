b3d082f3b8b8cc5d8e36d01a0d6c508f2b600992
ygdflgdflop <ashwatk2@illinois.edu>
2023-11-09 17:22:53 -0600

    Allowed apartment cards to pull from JSON file and added logo to navbar

--- apart-proj/src/App.js
+++ apart-proj/src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import About from './components/About';
-import Cards from './components/Cards';
+import Listings from './components/Listings';
 import './App.css'; // Create or import your main CSS file
+import Reviews from './components/Reviews';
 
 function App() {
   return (
     <Router>
       <div className="app">
         <nav className="navbar">
+          <Link to="/">
+            <img className="PClogo" src="https://projectcodeuiuc.org/project-code.svg" alt=""/>
+          </Link>
           <ul className="nav-list">
             <li className="nav-item">
               <Link to="/" className="nav-link">Home</Link>
             </li>
             <li className="nav-item">
-              <Link to="/cards" className="nav-link">Apartments</Link>
+              <Link to="/listings" className="nav-link">Listings</Link>
             </li>
             <li className="nav-item">
               <Link to="/about" className="nav-link">About</Link>
@@ -24,7 +28,8 @@ function App() {
 
         <Routes>
           <Route path="/about" element={<About />} />
-          <Route path="/cards" element={<Cards />} />
+          <Route path="/listings" element={<Listings />} />
+          <Route path="/reviews" element={<Reviews />} />
           <Route path="/" element={<div>Home Page Content</div>} />
         </Routes>
       </div>
